f2b807cd7d426e811e5fecf70b5cfa79fb248018
miyoalpizar <Miyo@DESKTOP-VOHF5HE>
2021-03-30 20:09:48 -0600

    Agregar archivos de proyecto.

---
+++ BooksApi/Controllers/AutoresController.cs
@@ -0,0 +1,74 @@
+﻿using BooksApi.Context;
+using BooksApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+
+        public AutoresController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Autor>>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "ObtenerAutor")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores.FindAsync(id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Autor autor)
+        {
+            context.Autores.Add(autor);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("ObtenerAutor", new { id = autor.Id }, autor);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Autor autor)
+        {
+            if (id != autor.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(autor).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Autor>> Delete(int id)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            context.Autores.Remove(autor);
+            await context.SaveChangesAsync();
+            return autor;
+        }
+    }
+}
